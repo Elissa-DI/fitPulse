999ffbb25a1dfacb0c1c1946cda2a2f4c9c2a231
Elissa <elissadusabe@gmail.com>
2023-11-23 17:22:26 +0200

    final changes for a home part

--- src/scenes/navbar/home/index.tsx
+++ src/scenes/navbar/home/index.tsx
@@ -20,52 +20,75 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section
       id="home"
-      className="gap-16 bg-gray-20 pb-10 pt-10 md:h-full md:pb-0"
+      className="gap-16 bg-gray-20 py-10 md:pt-20 md:h-full md:pb-0"
     >
       {/* Image and main header */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* Main header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <div className="md:-mt-20">
+          <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x:-50 },
+              visible: {  opacity:1, x: 0},
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm ">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
-          </div>
+          </motion.div>
           {/* Actions */}
-          <div>
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay:0.3, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x:-50 },
+              visible: {  opacity:1, x: 0},
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
             <AnchorLink
-              className = "text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-              onClick = {() => setSelectedPage(SelectedPage.ContactUs)}
-              href = {`${SelectedPage.ContactUs}`}
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`${SelectedPage.ContactUs}`}
             >
               Lear more
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         {/* Image */}
-        <div>
-          <img src={HomePageGraphic} alt="home-page_graphic" />          
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:t-16 md:justify-items-end">
+          <img src={HomePageGraphic} alt="home-page_graphic" />
         </div>
-      </div>
+      </motion.div>
       {/* Sponsors */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
-              <img src={SponsorRedBull} alt="Redbull" />
-              <img src={SponsorForbes} alt="Forbes" />
-              <img src={SponsorFortune} alt="Fortune" />
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
             </div>
           </div>
         </div>
